e7ea1af85803ec5e338ce156adfb242d85ad09f4
Ritesh Sharma <98305600+1224ritesh@users.noreply.github.com>
2023-07-09 18:42:45 +0530

    orders

---
+++ backend/controllers/converstation.controller.js
@@ -0,0 +1,42 @@
+import createError from "../utils/createError.js";
+import Conversation from "../models/conversation.model.js";
+
+export const createConversation = async(req, res, next) => {
+
+    const newConversation = new Conversation({
+
+    });
+
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+export const getConversations = async(req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+export const getSingleConversation = async(req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+export const updateConversation = async(req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err);
+    }
+};
